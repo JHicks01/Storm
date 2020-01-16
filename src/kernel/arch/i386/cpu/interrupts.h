59875c1abc9864288455b69efcb91093633edeff
James Hicks <jmhicks001@gmail.com>
2020-01-16 20:22:35 +0000

    Add documentation for interrupts.h API.

--- src/kernel/arch/i386/cpu/interrupts.h
+++ src/kernel/arch/i386/cpu/interrupts.h
@@ -3,10 +3,20 @@
 
 #include <stdint.h>
 
+/*
+ * IRQ handler function type.
+ *
+ * IRQ handlers take a single void * argument and return void.
+ */
 typedef void (*irq_handler_fn_t)(void *arg);
 
+/* Initialize interrupts.c module. */
 void interrupts_init(void);
 
+/*
+ * Enable IRQ number 'irq' and register an IRQ handler to service that
+ * interrupt.
+ */
 void interrupts_register_irq(uint8_t irq, irq_handler_fn_t fn, void *arg);
 
 #endif /* _INTERRUPTS_H_ */
